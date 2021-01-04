19925d85e04492ab7aed29ca9470ddddfb84611f
tongchengbin <58296672@qq.com>
2021-01-04 10:16:12 +0800

    first commit

---
+++ src/api/user.js
@@ -0,0 +1,23 @@
+import request from '@/utils/request'
+
+export function login(data) {
+  return request({
+    url: '/api/admin/login',
+    method: 'post',
+    data
+  })
+}
+
+export function getInfo() {
+  return request({
+    url: '/api/admin/admins/info/',
+    method: 'get'
+  })
+}
+
+export function logout() {
+  return request({
+    url: '/api/admin/logout',
+    method: 'post'
+  })
+}
---
+++ src/utils/request.js
@@ -0,0 +1,65 @@
+import axios from 'axios'
+import { MessageBox, Message } from 'element-ui'
+import store from '@/store'
+import { getToken } from '@/utils/auth'
+
+// create an axios instance
+const service = axios.create({
+  baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
+  // withCredentials: true, // send cookies when cross-domain requests
+  timeout: 5000 // request timeout
+})
+
+// request interceptor
+service.interceptors.request.use(
+  config => {
+    if (store.getters.token) {
+      config.headers['token'] = getToken()
+    }
+    return config
+  },
+  error => {
+    // do something with request error
+    console.log(error) // for debug
+    return Promise.reject(error)
+  }
+)
+
+// response interceptor
+service.interceptors.response.use(
+  /**
+   * If you want to get http information such as headers or status
+   * Please return  response => response
+  */
+
+  /**
+   * Determine the request status by custom code
+   * Here is just an example
+   * You can also judge the status by HTTP Status Code
+   */
+  response => {
+    return response.data
+  },
+  error => {
+    if (error.response) {
+      Message({
+        message: error.response.data.msg,
+        type: 'error',
+        duration: 5 * 1000
+        // eslint-disable-next-line no-empty
+      })
+    } else {
+      Message({
+        message: '网络错误!',
+        type: 'error',
+        duration: 5 * 1000
+        // eslint-disable-next-line no-empty
+      })
+
+    }
+
+    return Promise.reject(error)
+  }
+)
+
+export default service
